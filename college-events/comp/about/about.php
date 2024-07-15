1112c965bbf3c8cae9c3b7eb7e4b03797141e5d0
harshkavaiya <hkavaiya001@gmail.com>
2024-07-15 10:40:21 +0530

    Add files via upload

---
+++ college-events/comp/about/about.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="comp/about/about.css">
+<div class="about" id="about">
+    <div class="left">
+        <img src="images/about-1.jpg" alt="" class="about-img">
+        <img src="images/play.png" alt="" class="play">
+    </div>
+    <div class="right">
+        <h3>ABOUT UNIVERSITY</h3>
+        <h2>Nururing Tommorrow's Leading Today</h2>
+        <p>
+            Bhavnagar is traditionally known for its cultural heritage in the western India with its last princely ruler
+            Shri Krishnakumarsinhji who was the first to donate his princely state to the Nation. Mahatma Gandhi, the
+            father of the nation received his initial college education in Shamaldas College during 1888, which is today
+            run by Maharaja Krishnakumarsinhji Bhavnagar University. Bhavnagar District is situated between 21.18-22.18
+            Longitude and 71.51-72.18 Latitude.
+        </p>
+        <p>The University was established way back in 1978 by the state of Gujarat and the total area of the campus is
+            269 acres, spread over 3 location. The University is providing high quality of Teaching and Research
+            Programmes in faculties as diverse as Science, Arts, Commerce, Engineering, Medicine, Management, Rural
+            Studies and Law. The University has at present 20 Post Graduate Departments and 4 Recognized Institutes.</p>
+    </div>
+</div>
---
+++ college-events/comp/navbar/navbar.php
@@ -0,0 +1,16 @@
+<link href="comp/navbar/navbar.css" rel="stylesheet" />
+
+<nav class="container">
+    <img src="./images/logo.png" alt="logo" class="logo" />
+    <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#program">program</a></li>
+        <li><a href="#about">about us</a></li>
+        <li><a href="#campus">campus</a></li>
+        <li><a href="#testi">testimonia</a></li>
+        <li>
+            <button class="btn"><a href="#contact">Contact Us</a></button>
+        </li>
+    </ul>
+</nav>
+<script src="comp/navbar/navbar.js"></script>
