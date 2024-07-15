0f176bdb0ffb6d9a01777aaaefff2a6d72f16c34
harshkavaiya <hkavaiya001@gmail.com>
2024-07-15 10:40:44 +0530

    Delete weddding directory

--- weddding/comp/title/title.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="comp/title/title.css">
-<div class="title">
-    <p><?php echo htmlspecialchars($title);?></p>
-    <h2><?php echo htmlspecialchars($sub);?></h2>
-</div>
