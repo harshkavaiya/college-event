1b6d4f092d9488cf7b7a8d5a9fd399fa76df0e4f
harshkavaiya <hkavaiya001@gmail.com>
2024-08-08 20:10:16 +0530

    Delete college-events directory

--- college-events/comp/videoplay/video.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="comp/videoplay/video.css">
-
-<div class="video-player hide">
-    <video src="images/video.mp4" autoplay muted controls></video>
-</div>
-<script src="comp/videoplay/video.js"></script>
